767169be2738335dfa0ac923834f6846f56091b5
dennis <dp.jandow@gmail.com>
2024-06-06 12:13:28 +0200

    add http request for authentication

--- scorpio/src/extension.ts
+++ scorpio/src/extension.ts
@@ -3,6 +3,7 @@
 import * as vscode from 'vscode';
 import { ArtemisPanel } from './artemisPanel';
 import { SidebarProvider } from './sidebarProvider';
+import { authenticate } from './authentication';
 
 // npm run compile
 
@@ -22,44 +23,21 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('scorpio.openWebview', () => {
+	let disposable = 
 
-
+	context.subscriptions.push(vscode.commands.registerCommand('scorpio.openWebview', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Opening Artemis');
 		// Create and show a new webview
 		ArtemisPanel.createOrShow(context.extensionUri);
+	}));
 
-		// const panel = vscode.window.createWebviewPanel(
-		// 	'artemisWebview', // Identifies the type of the webview. Used internally
-		// 	'Artemis Panel', // Title of the panel displayed to the user
-		// 	vscode.ViewColumn.One, // Editor column to show the new webview panel in.
-		// 	{
-		// 		// Enable scripts in the webview
-		// 		enableScripts: true,
-		// 	}
-		// );
-
-		// Set the content of the webview
-		//panel.webview.html = getWebviewContent();
-	});
-
-	context.subscriptions.push(disposable);
-}
-
-function getWebviewContent() {
-    return `<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Artemis Panel</title>
-    </head>
-    <body>
-        <iframe src="https://artemis.cit.tum.de" width="100%" height="100%"></iframe>
-    </body>
-    </html>`;
+	context.subscriptions.push(
+		vscode.commands.registerCommand('scorpio.authenticate', () => {
+			vscode.window.showInformationMessage('Start Authentication');
+			authenticate("artemis_admin", "artemis_admin");
+	}));
 }
 
 // This method is called when your extension is deactivated
