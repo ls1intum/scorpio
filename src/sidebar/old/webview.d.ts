a44770e038beebf91d18f4b879f9cd924c381594
dennis <dp.jandow@gmail.com>
2024-11-09 23:45:28 +0100

    remove old sidebar

--- src/sidebar/old/webview.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '*.html' {
-    const content: string;
-    export default content;
-}
-
-declare module "!raw-loader!*" {
-    const content: string;
-    export default content;
-  }
